c7a42a202f3760beb416a09233a55c8398af7d1e
cocoyes <moy8525@gmail.com>
2021-12-16 18:49:11 +0800

    add nft obj

---
+++ src/main/java/com/dechain/msg/coin/NFTMarketItem.java
@@ -0,0 +1,49 @@
+package com.dechain.msg.coin;
+
+public class NFTMarketItem {
+    private String img; //图集图片
+    private String title; //图集标题
+    private String content; //图集描述
+    private String contract; //NFT合约
+    private String creator; //发行人
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getContract() {
+        return contract;
+    }
+
+    public void setContract(String contract) {
+        this.contract = contract;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+}
